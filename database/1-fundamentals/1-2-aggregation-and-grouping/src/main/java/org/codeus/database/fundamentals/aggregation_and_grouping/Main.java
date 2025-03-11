a06a6d60ecde09276f5d03b774938cb4cc1586a8
skravchuk <ext-serhii.krachuk@here.com>
2025-03-11 14:41:13 +0200

    added project and module structures for the first sprint of the Deliberate Practice

---
+++ database/1-fundamentals/1-2-aggregation-and-grouping/src/main/java/org/codeus/database/fundamentals/aggregation_and_grouping/Main.java
@@ -0,0 +1,7 @@
+package org.codeus.database.fundamentals.aggregation_and_grouping;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ database/1-fundamentals/1-3-joins-and-subqueries/src/main/java/org/codeus/database/fundamentals/joins_and_subqueries/Main.java
@@ -0,0 +1,7 @@
+package org.codeus.database.fundamentals.joins_and_subqueries;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}

9a814ccc3733a04b9a4c1dc3e6878fd4c10bf825
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2025-03-20 09:11:06 +0200

    1-2-aggregation-and-grouping: Fixed description to test and updated presentation

--- database/1-fundamentals/1-2-aggregation-and-grouping/src/test/resources/queries/optional/04_min_max/32_min_max_loan_amounts.sql
+++ database/1-fundamentals/1-2-aggregation-and-grouping/src/test/resources/queries/optional/04_min_max/32_min_max_loan_amounts.sql
@@ -1,4 +1,4 @@
--- 32: Find the lowest and highest loan amounts for active loans only
+-- 32: Find the lowest and highest loan amounts for active loans with customer IDs in the range of 5 to 10
 SELECT MIN(amount) AS smallest_loan,
        MAX(amount) AS largest_loan
 FROM loans
